aaf596a9d6ddf36cc2587c0fdc759059a95be033
Dmytro Basamyhin <dima17lit@gmail.com>
2025-10-04 10:44:19 +0300

    fix

--- src/models/note.js
+++ src/models/note.js
@@ -5,15 +5,16 @@ const noteSchema = new Schema(
   {
     title: {
       type: String,
+      required: true,
       trim: true
     },
     content: {
       type: String,
-      trim: true
+      trim: true,
+      default:" "
     },
     tag: {
       type: String,
-      required: true,
       enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
       default: "Todo"
     },
